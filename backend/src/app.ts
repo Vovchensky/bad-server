521619b322da2b6f9f10857d3711242ec61ee3e8
Vovchensky <vovchensky.pt@bk.ru>
2026-05-06 19:41:16 +0700

    fix: DDoS - add rate limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
+import rateLimit from 'express-rate-limit'
 import mongoose from 'mongoose'
 import path from 'path'
 import { DB_ADDRESS } from './config'
@@ -13,23 +14,24 @@ import routes from './routes'
 const { PORT = 3000 } = process.env
 const app = express()
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: { message: 'Слишком много запросов, попробуйте позже' },
+})
+
+app.use(limiter)
 app.use(cookieParser())
-
 app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(serveStatic(path.join(__dirname, 'public')))
-
 app.use(urlencoded({ extended: true }))
 app.use(json())
-
 app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
-
-// eslint-disable-next-line no-console
 
 const bootstrap = async () => {
     try {
